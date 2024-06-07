861d97dc7da5c042d84f1539bf945081cd2c6d32
=Ali Alhashim <ali-alhashim@outlook.com>
2024-06-07 15:54:11 +0300

    add searchbox to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,15 @@
+import {  SearchBox } from "@fluentui/react-components"
 export const Navbar = () => {
     return(
         <div className="navbar">
            <div className="navbar-left-items">
-              <h2>HeroTech</h2>
+              <h2>HeroTech | ERP</h2>
            </div>
 
            <div className="navbar-center-items">
-
+           
+             <SearchBox />
+           
            </div>
 
            <div className="navbar-right-times">
